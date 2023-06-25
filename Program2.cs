156db55cabfed27eeada3aba6b5746dfb4abe5b4
Jon Cundiff <jmc9.1987@gmail.com>
2023-06-24 22:15:08 -0400

    Created Program2.cs and added pseudocode for mainloop

---
+++ Program2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaundrySimulator2
+{
+    public class Program2 
+    {
+        static void Main(string[] args) //for now this is the beginning of my new main loop
+        {
+            Console.Title = "Laundry Simulator Redux"; 
+            //pseudo code onward
+            show title
+            wait a few seconds
+
+            call the mainMenu //mainMenu will a class of some kind
+            after selection, display whatever corresponds with option chosen
+            to know what to do have a SWITCH block that will call the requested output  //playing the game, instruction, quitting, etc will be the outputs
+            
+            
+
+        }
+    }
+}
