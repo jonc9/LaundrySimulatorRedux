b87582bb1734f45d7ed8ae97967244ac73407854
Jon Cundiff <jmc9.1987@gmail.com>
2023-07-26 20:43:49 -0400

    added a prompt to get the player's name to store it in the database

--- Program2.cs
+++ Program2.cs
@@ -1,18 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static LaundrySimulator2.MainMenu;
-namespace LaundrySimulator2
+﻿namespace LaundrySimulator2
 {
-    public class Program2 
+    public class Program2
     {
         static void Main(string[] args) //for now this is the beginning of my new main loop
         {
             Console.Title = "Laundry Simulator Redux";
 
             MainMenu MainMenu = new MainMenu();
+
+            using var db = new NameContext(); //look into this to see  if i need to change this 
+
+            //this is where the player name will be asked and put in a table
+            Console.WriteLine("What is your name?");
+            db.Add(new PlayerName { });
+            db.SaveChanges();
+
+            //figure out how to pick up what was chosen in mainmenu and go from there if ()
+            //{
+
+            //}
+
+
+
 
             //bool showMenu = true;
             //while (true)
@@ -27,7 +36,7 @@ namespace LaundrySimulator2
         //    call the mainMenu //mainMenu will a class of some kind
         //    after selection, display whatever corresponds with option chosen
         //    to know what to do have a SWITCH block that will call the requested output  //playing the game, instruction, quitting, etc will be the outputs
-            
+
         //    when mainMenu is finished, bring forth appropriate menu such as GamePlayMenu
 
         //}
