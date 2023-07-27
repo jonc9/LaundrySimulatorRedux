0345bff8c46bbd8f68bd4851a9fa6e4786502fd4
Jon Cundiff <jmc9.1987@gmail.com>
2023-07-26 20:57:12 -0400

    changed the name of model.cs to NameContext.cs

---
+++ NameContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LaundrySimulator2
+{
+    public class NameContext : DbContext
+    {
+        public DbSet<PlayerNames> PlayerName { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = LaundrySimulatorReduxDataBase");
+        }
+    }
+
+    public class PlayerName // will probably have to put this into its own file at some point
+    {
+        public int NameId { get; set; }
+    }
+}
+
