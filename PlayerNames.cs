ba9f81f99e0f170a4d21a81fa43322d926dbd3a1
Jon Cundiff <jmc9.1987@gmail.com>
2023-08-01 19:26:17 -0400

    Removed PlayerName class inheritance of NameContext

--- PlayerNames.cs
+++ PlayerNames.cs
@@ -2,7 +2,7 @@
 
 namespace LaundrySimulator2
 {
-    public class PlayerName : NameContext
+    public class PlayerName
     {
         [Key]
         public int NameId { get; set; } // 7-26-2023: NEED TO HAVE THIS BE USED TO ACCEPT USER INPUT FOR THE NAME.
