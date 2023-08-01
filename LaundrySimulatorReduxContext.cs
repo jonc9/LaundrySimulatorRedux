114b5a9dd15b52cf5145cb29fa8a4ea119df8a50
Jon Cundiff <jmc9.1987@gmail.com>
2023-08-01 19:33:34 -0400

    Updated the file name, class name to better reflect a more general context for when I add more DbSets.

---
+++ LaundrySimulatorReduxContext.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace LaundrySimulator2
+{
+    public class LaundrySimulatorReduxContext : DbContext
+    {
+        public DbSet<PlayerName> PlayerNames { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = LaundrySimulatorReduxDataBase");
+        }
+    }
+}
+
