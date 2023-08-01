591cba17f8a5f8e6199546a018054339254b93bf
Jon Cundiff <jmc9.1987@gmail.com>
2023-08-01 17:54:19 -0400

    most recent

--- NameContext.cs
+++ NameContext.cs
@@ -1,21 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.ComponentModel;
+using Microsoft.EntityFrameworkCore;
 
 namespace LaundrySimulator2
 {
     public class NameContext : DbContext
     {
-        public DbSet<PlayerNames> PlayerName { get; set; }
+        public DbSet<PlayerName> PlayerNames { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = LaundrySimulatorReduxDataBase");
         }
-    }
-
-    public class PlayerName // will probably have to put this into its own file at some point
-    {
-        public int NameId { get; set; } // 7-26-2023: NEED TO HAVE THIS BE USED TO ACCEPT USER INPUT FOR THE NAME.
-        public string Name { get; set; }
     }
 }
 
--- PlayerNames.cs
+++ PlayerNames.cs
@@ -1,6 +1,11 @@
-﻿namespace LaundrySimulator2
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LaundrySimulator2
 {
-    public class PlayerNames
+    public class PlayerName : NameContext
     {
+        [Key]
+        public int NameId { get; set; } // 7-26-2023: NEED TO HAVE THIS BE USED TO ACCEPT USER INPUT FOR THE NAME.
+        public string Name { get; set; }
     }
 }
