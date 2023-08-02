cd5dac9eb75b506171baace3d75196126fa56fa0
Jon Cundiff <jmc9.1987@gmail.com>
2023-08-01 21:25:54 -0400

    removed [key] above public string NameId; changed string to int for public int NameId; added [PrimaryKey(nameof(NameId))] above public class PlayerName

--- PlayerNames.cs
+++ PlayerNames.cs
@@ -1,11 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace LaundrySimulator2
 {
+    [PrimaryKey(nameof(NameId))]
     public class PlayerName
     {
-        [Key]
-        public string NameId { get; set; } // 7-26-2023: NEED TO HAVE THIS BE USED TO ACCEPT USER INPUT FOR THE NAME.
+        public int NameId { get; set; } // 7-26-2023: NEED TO HAVE THIS BE USED TO ACCEPT USER INPUT FOR THE NAME.
         public string Name { get; set; }
     }
 }
