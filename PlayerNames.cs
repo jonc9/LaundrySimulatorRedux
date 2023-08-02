ecfd2b631369f738bdb909f3c829fbf72ac861f4
Jon Cundiff <jmc9.1987@gmail.com>
2023-08-01 20:06:14 -0400

    Changed NameId to string from int

--- PlayerNames.cs
+++ PlayerNames.cs
@@ -5,7 +5,7 @@ namespace LaundrySimulator2
     public class PlayerName
     {
         [Key]
-        public int NameId { get; set; } // 7-26-2023: NEED TO HAVE THIS BE USED TO ACCEPT USER INPUT FOR THE NAME.
+        public string NameId { get; set; } // 7-26-2023: NEED TO HAVE THIS BE USED TO ACCEPT USER INPUT FOR THE NAME.
         public string Name { get; set; }
     }
 }
