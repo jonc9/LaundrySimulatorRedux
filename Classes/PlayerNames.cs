c3cccf50b47ae21938c2b1d4512f8a51271b0245
Jon Cundiff <jmc9.1987@gmail.com>
2023-08-02 21:42:28 -0400

    Added a Classes folder and moved PlayerNames.cs to it.

---
+++ Classes/PlayerNames.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LaundrySimulator2.Classes
+{
+    public class PlayerName //this is what gets the player name and stores it
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+    }
+}
