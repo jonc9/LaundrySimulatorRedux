2a9529a171da1db54527efe717516da30c2fd99d
Jon Cundiff <jmc9.1987@gmail.com>
2023-08-04 10:58:07 -0400

    Cleaned up some of the code

--- LaundrySimulatorReduxContext.cs
+++ LaundrySimulatorReduxContext.cs
@@ -6,13 +6,15 @@ namespace LaundrySimulator2
     public class LaundrySimulatorReduxContext : DbContext //***ADD METHOD FOR SETTING UP INITIAL MIGRATION AS SOON AS APPLICATION STARTS***
     {
         public DbSet<PlayerName> PlayerNames { get; set; }
+        
+        public DbSet<DefaultInventory> DefaultInventories { get; set; }
+
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = LaundrySimulatorReduxDataBase");
         }
 
-        public DbSet<DefaultInventory> DefaultInventories { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder) //this is for seeding
         {
@@ -27,7 +29,15 @@ namespace LaundrySimulator2
 
 
             modelBuilder.Entity<DefaultInventory>().HasData(
-                new DefaultInventory { InvId = 1, ItemName = "Mallet", ItemDescription = "Test description" }//update descriptions later
+                new DefaultInventory { InvId = 1, ItemName = " - Laundry Detergent", ItemDescription = "Run of the mill variety detergent;" },
+                new DefaultInventory { InvId = 2, ItemName = " - Wallet", ItemDescription = "Your wallet. Tearing at the seams. Only a couple quarters;" },
+                new DefaultInventory { InvId = 3, ItemName = " - JNCO Jeans", ItemDescription = "Something from your youth. How did they get here?;" },
+                new DefaultInventory { InvId = 4, ItemName = " - Sailor Moon T-shirt", ItemDescription = "Black t-shirt with Sailor Saturn on it since she's your favorite;" },
+                new DefaultInventory { InvId = 5, ItemName = " - Polo shirt with your name on it.", ItemDescription = "Basic purple polo shirt you got as a gift;" }
+
+
+                //new DefaultInventory { InvId = 100, ItemName = "Mallet", ItemDescription = "Test description" }//update descriptions later
+
         );
         }        
     }
