6e837f1190dd3de13cffb78c89d3d56dfd3ce2f4
Jon Cundiff <jmc9.1987@gmail.com>
2023-08-04 07:03:24 -0400

    This file was created when extracting this class from the LaundrySim DbContext file; also moved file to Classes folder.

---
+++ Classes/DefaultInventory.cs
@@ -0,0 +1,15 @@
+﻿namespace LaundrySimulator2.Classes
+{
+    public class DefaultInventory // This is the class to set what the default (aka all possible items) has. *** may eventually convert to Interface*** 
+    {
+        public int InvId { get; set; }
+        public required string ItemName { get; set; }
+        public string ItemDescription { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {ItemName}, Description: {ItemDescription}";
+        }
+    }
+}
+
