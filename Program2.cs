0d524ec907ba5bd32a2b42b146b2bf0022fa2bb5
Jon Cundiff <jmc9.1987@gmail.com>
2023-08-04 08:24:39 -0400

    Working on MainLoop()

--- Program2.cs
+++ Program2.cs
@@ -10,20 +10,20 @@ namespace LaundrySimulator2
         {
             Console.Title = "Laundry Simulator Redux";
 
-            MainMenu MainMenu = new MainMenu(); //calls MainMenu.cs
+            //MainMenu MainMenu = new MainMenu(); //calls MainMenu.cs ***REMOVE IF THIS WORKS IN MAIN LOOP BELOW***
 
             using var db = new LaundrySimulatorReduxContext(); //look into this to see  if i need to change this 
-            
+
             //using (NameContext context = new NameContext()) **may keep this since its just a console application***
             //{
             //    context.Database.EnsureCreated();
             //}
 
-            GetPlayerName();
-            Thread.Sleep(100);
-            Console.Clear();
-            
-            DefaultInvTest();
+            //GetPlayerName();
+            //Thread.Sleep(100);
+            //Console.Clear();
+            MainLoop();
+            //DefaultInvTest();
 
             void GetPlayerName() // this will eventually be moved into its own class or file
             {
@@ -35,17 +35,38 @@ namespace LaundrySimulator2
                 context.SaveChanges();             
             }
 
-            void DefaultInvTest() //***FIGURE OUT HOW TO GET THE SEEDED TABLE TO DISPLAY ON COMMAND***
+            void DefaultInvTest() //DON'T FORGET TO REFACTOR METHOD AND CHANGE TO REFLECT THE PLAYER INVENTORY
             {
-                Console.WriteLine("This is a test to see if the Default Inventory table appears");
-                //LaundrySimulatorReduxContext laundrySimulatorReduxContext = new LaundrySimulatorReduxContext();
+                Console.WriteLine("This is a test to see if the Default Inventory table appears"); // DON'T FORGET TO CHANGE 
                 using var context = new LaundrySimulatorReduxContext();
-                var defaultinv = context.DefaultInventories;
+                var defaultinv = context.DefaultInventories; //CHANGE defaultinv to appropriate table name from lines 42 to 45!
                 foreach (var defaultInventories in defaultinv)
                 {
                     Console.WriteLine(defaultInventories);
                 }
             }
+
+            void MainLoop()
+            {
+                MainMenu mainMenu = new MainMenu(); //don't forget to clean up MainMenu
+                Intro();
+                GetPlayerName();
+
+
+            }
+
+            void Intro()
+            {
+                Console.WriteLine("Finally home from work, you stumble into the door, elated. You toss your keys and other personal items in a corner somewhere, kick your shoes off like Kevin Bacon and head toward your bedroom. Your bed is looking inviting. Calling your name. Before you can give your bed a hug is when you see it...\n\n\n");
+                Thread.Sleep(3000); //look for better alternative such as delay or await
+                Console.WriteLine("Your dirty laundry in the corner! 'Could this day get any worse...', You think to yourself as your begrudgingly drag yourself to all your laundry and grab it, grab your laundry detergent and wallet (after searching for it since you tossed it and headed toward your apartment's laundry area....\n\n\n");
+                Thread.Sleep(3000);
+                Console.WriteLine("However... you weren't prepared for what awaited you...");
+                Thread.Sleep(1000);
+                Console.WriteLine("Press any key to continue.");
+                Console.ReadKey();
+            }
+
 
             //***GAME LOOP WILL PROCEED HERE***
 
